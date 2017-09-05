5b029ea4bdd8a5663c5bfeb70f8ca1a3d1b2ad8e
chazzy666 <31612338+chazzy666@users.noreply.github.com>
2017-09-05 11:39:36 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package com.agung.abdul.rahman.belajar.java;
+
+public class Hello
+{
+	/**
+	* My first java program
+	*/
+	public static void main(String[] args) {
+	 //menampilkan string”Hello world” pada screen
+	
+		System.out.println("Hello world!");
+	}
+}
